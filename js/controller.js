1d518f29565d4a24e88aa6fe50b00900f1888532
Kyle Bachmann <kylebachmann@Hotmail.com>
2013-11-17 18:17:23 -0800

    ALl edits

--- js/controller.js
+++ js/controller.js
@@ -3,6 +3,66 @@
 */
 
 $(function(){
+	var formatLabels = {
+		dvd: 'DVD',
+		bluray: 'Blu-Ray'
+	};
+	var cartModel = createCartModel();
+
+	var cartView = createCartView({
+		model: cartModel,
+		template: $('.cart-item-template'),
+		container: $('.cart-items-container'),
+		totalPrice: $('.total-price')
+	});
+
+	var moviesModel = createMoviesModel({
+    	url: 'https://courses.washington.edu/info343/ajax/movies/'
+	});
+
+	var moviesView = createMoviesView({
+	    model: moviesModel,
+	    template: $('.movie-template'),
+	    container: $('.movies-container')
+	});
+
+	moviesModel.refresh();
+
+	moviesView.on('addToCart', function(data){
+	    var movie = moviesModel.getItem(data.movieID);
+	    if (!movie)
+	        throw 'Invalid movie ID "' + movieID + '"!'; 
+
+	    cartModel.addItem({
+	        id: movie.id,
+	        title: movie.title,
+	        format: data.format,
+	        formatLabel: formatLabels[data.format],
+	        price: movie.prices[data.format]
+	    });
+	}); //addToCart event
+
+	$('.place-order').click(function(){
+		$.ajax({
+		    url: 'https://courses.washington.edu/info343/ajax/movies/orders/',
+		    type: 'POST',
+		    data: cartModel.toJSON(),
+		    contentType: 'application/json',
+		    success: function(responseData) {
+		        var resp = responseData.message;
+		        alert(resp);
+		        cartModel.setItems([]);
+
+		    },
+		    error: function(jqXHR, status, errorThrown) {
+		        //error with post--alert user
+		        alert(errorThrown || status);
+		    }
+		}); //ajax()
+	});
+
+
+
 
 }); //doc ready()
 
